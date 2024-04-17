e6276d2014a27b78ebe4efd2f1932005c8a6b7fe
Darksparkx <ar.danish800@gmail.com>
2024-04-17 17:55:14 +0530

    reverse emp by gender change

--- MySQL/emp_by_gender.sql
+++ MySQL/emp_by_gender.sql
@@ -3,13 +3,15 @@ working in the company each year, starting from 1990 */
 
 /*CSV Output: output/emp_by_gender.csv*/
 SELECT 
-    DATE_FORMAT(d.from_date, "%Y") AS calendar_year,
-    SUM(CASE WHEN e.gender = "F" THEN 1 ELSE 0 END) AS female,
-    SUM(CASE WHEN e.gender = "M" THEN 1 ELSE 0 END) AS male
+    DATE_FORMAT(d.from_date,"%Y") AS calender_year,
+    CASE
+		WHEN e.gender = "M" THEN "Male"
+        ELSE "Female"
+    END AS gender,
+    COUNT(e.gender) AS number_of_employees
 FROM employees e
 JOIN dept_emp d ON e.emp_no = d.emp_no
-GROUP BY 1
-HAVING calendar_year >= 1990
-ORDER BY 1;
-
+GROUP BY 1,2
+HAVING calender_year >= 1990
+ORDER BY 1,2;
 
